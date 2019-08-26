ce1b051d93a48633c291c8ee70b439804450eeda
shieldofzues <shieldofzues@hotmail.com>
2019-08-26 09:33:08 +0800

    exception处理

--- demo/src/main/java/com/example/demo/controller/UserController.java
+++ demo/src/main/java/com/example/demo/controller/UserController.java
@@ -6,38 +6,41 @@ import com.example.demo.util.ResultInfoVO;
 import com.example.demo.util.ResultMessage;
 import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @Author shieldofzues
  * @Description 用户管理
  * @Date 16:50 2016/5/23
- *
  **/
 @Api("用户管理")
 @RestController
 @RequestMapping("/user")
 public class UserController {
-	@Autowired
-	private UserService userService;
+    @Autowired
+    private UserService userService;
 
-	@ApiOperation("新增用户")
-	@ApiImplicitParam(name="user", value="单个用户", required = true, dataType = "User")
-	@ApiResponses({
-			@ApiResponse(code = 400, message = "请求参数没填好"), // 响应对应编码的描述
-			@ApiResponse(code = 404, message = "请求路径没有或页面跳转路径不对")
-	})
+    @ApiOperation("新增用户")
+    @ApiImplicitParam(name = "user", value = "单个用户", required = true, dataType = "User")
+    @ApiResponses({
+            @ApiResponse(code = 400, message = "请求参数没填好"), // 响应对应编码的描述
+            @ApiResponse(code = 404, message = "请求路径没有或页面跳转路径不对")
+    })
+    /**
+     *  @Validated({Severitys.Update.class}) 可以做类似这种的分组校验
+     */
 
-
-	@PostMapping("/insertUser")
-	public ResultInfoVO insertSelective(@RequestBody User user) {
-		if (userService.insertSelective(user) > 0) {
-			return new ResultInfoVO(ResultMessage.SUCCESS.code, null, ResultMessage.SUCCESS.message);
-		} else {
-			return new ResultInfoVO(ResultMessage.ERROR.code, null, ResultMessage.ERROR.message);
-		}
-	}
+    @PostMapping("/insertUser")
+    public ResultInfoVO insertSelective(@RequestBody @ApiParam(value = "新增用户实体") @Validated User user) {
+        if (userService.insertSelective(user) > 0) {
+            return new ResultInfoVO(ResultMessage.SUCCESS.code, null, ResultMessage.SUCCESS.message);
+        } else {
+            return new ResultInfoVO(ResultMessage.ERROR.code, null, ResultMessage.ERROR.message);
+        }
+    }
 
 }
--- demo/src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ demo/src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -5,27 +5,32 @@ import com.example.demo.util.ResultInfoVO;
 import com.example.demo.util.ResultMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Iterator;
+
 /**
  * @Author shieldofzues
  * @Description 统一处理
  * @Date 11:10 2016/5/23
- *
  **/
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-	private final static Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+    private final static Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
-	/**
-	 * 判断
-	 */
-	public static boolean isAjax(HttpServletRequest httpRequest) {
+    /**
+     * 判断
+     */
+	/*public static boolean isAjax(HttpServletRequest httpRequest) {
 		return (httpRequest.getHeader("X-Requested-With") != null
 				&& "XMLHttpRequest"
 				.equals(httpRequest.getHeader("X-Requested-With").toString()));
@@ -53,5 +58,51 @@ public class GlobalExceptionHandler {
 				return jsonResult;
 			}
 		}
-	}
+	}*/
+
+    /**
+     * 以下异常处理可以处理controller层 参数校验产生的异常
+     */
+    @ExceptionHandler(BindException.class)
+    @ResponseBody
+    public ResultInfoVO exceptionHandler(BindException ex) {
+        BindingResult result = ex.getBindingResult();
+        return processBindingResult(result);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseBody
+    public ResultInfoVO exceptionHandler(MethodArgumentNotValidException ex) {
+        BindingResult result = ex.getBindingResult();
+        return processBindingResult(result);
+    }
+
+    private ResultInfoVO processBindingResult(BindingResult result) {
+        ResultInfoVO resultInfoVO = new ResultInfoVO();
+        if (result.hasErrors()) { // 现在表示执行的验证出现错误
+            String errorMessage = "";
+            Iterator<ObjectError> iterator = result.getAllErrors().iterator(); // 获取全部错误信息
+            while (iterator.hasNext()) {
+                ObjectError error = iterator.next();    // 取出每一个错误
+                errorMessage += "[" + error.getDefaultMessage() + "]";
+            }
+            resultInfoVO.setCode(ResultMessage.ERROR.code);
+            resultInfoVO.setMessage(errorMessage);
+        }
+        return resultInfoVO;
+    }
+
+    @ExceptionHandler(value = Exception.class)
+    @ResponseBody
+    public ResultInfoVO errorHandler(HttpServletRequest reqest,
+                                     HttpServletResponse response, Exception e) throws Exception {
+        if (e instanceof CustomException) {
+            CustomException customException = (CustomException) e;
+            return new ResultInfoVO(customException.getCode(), null, customException.getMessage());
+        } else {
+            logger.error(String.valueOf(ResultMessage.SYS_ERROR.code), e);
+            return new ResultInfoVO(ResultMessage.SYS_ERROR.code, null, ResultMessage.SYS_ERROR.message);
+        }
+    }
+
 }
