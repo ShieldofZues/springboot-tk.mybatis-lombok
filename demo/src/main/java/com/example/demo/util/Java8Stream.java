c536db49e0228407c5832a4f856b3ba1bacc958b
shieldofzues <shieldofzues@hotmail.com>
2019-09-04 20:09:49 +0800

    有意思的需求+有意思的编码

--- demo/src/main/java/com/example/demo/util/Java8Stream.java
+++ demo/src/main/java/com/example/demo/util/Java8Stream.java
@@ -1,7 +1,10 @@
 package com.example.demo.util;
 
+import com.example.demo.entity.User;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.toList;
 
@@ -20,12 +23,28 @@ public class Java8Stream {
         list2.add("333");
         list2.add("777");
         list2.add("888");
-
+        List<User> list3 = new ArrayList<User>();
+        User user = new User();
+        user.setId(11);
+        user.setInfo("22");
+        user.setName("33");
+        list3.add(user);
+        List<User> list4 = new ArrayList<User>();
+        User user1 = new User();
+        user1.setId(22);
+        user1.setInfo("22");
+        user1.setName("33");
+        list4.add(user1);
+        //两个集合比较 如果ID相等我就认为这俩相等 注：与contains不同  如果非要用contains方式实现 可以重写equals、hashcode的方式实现
+        list4 = list4.stream().filter(
+                item -> list3.stream().allMatch(
+                        item2 -> !item.getId().equals(item2.getId())))
+                .collect(Collectors.toList());
+        list4.parallelStream().forEach(System.out::println);
         // 取两个集合交集
         List<String> intersection = list1.stream().filter(item -> list2.contains(item)).collect(toList());
         System.out.println("---交集 intersection---");
         intersection.parallelStream().forEach(System.out::println);
-
         // 取两个集合差集 (list1 - list2)
         List<String> reduce1 = list1.stream().filter(item -> !list2.contains(item)).collect(toList());
         System.out.println("---差集 reduce1 (list1 - list2)---");
